8dbff2e4d715da089271a5350aaffc398174aa7c
arsaizdihar <arsadihar@gmail.com>
2023-04-07 18:33:05 +0700

    Add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,18 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        circular: ['CircularStd', ...defaultTheme.fontFamily.sans],
+      }
+    },
   },
-  // eslint-disable-next-line no-undef
   plugins: [require('daisyui')],
 }
