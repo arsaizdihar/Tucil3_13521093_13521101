cc481c034ef02bdf95fd306579c60603fb95ae9f
arsaizdihar <arsadihar@gmail.com>
2023-04-07 18:28:51 +0700

    Add daisy UI

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,11 +1,13 @@
 
 function MainPage({navigate}: {navigate: (path: string) => void}) {
   return (
-    <div className='flex flex-col'>
-      <h1 className="">Tugas Kecil 3 Strategi Algoritma</h1>
-      <p>Algoritma UCS dan A*</p>
-      <button onClick={() => navigate('normal')}>Normal</button>
-      <button onClick={() => navigate('gmap')}>Google Map</button>
+    <div className='flex flex-col max-w-sm mx-auto justify-center items-center h-screen w-full'>
+      <h1 className="text-white text-xl font-bold">Tugas Kecil 3 Strategi Algoritma</h1>
+      <p className="text-gray-200 text-lg font-medium mt-2">Algoritma UCS dan A*</p>
+      <div className="mt-8 w-full max-w-xs space-y-4">
+        <button onClick={() => navigate('normal')} className="btn btn-primary w-full">Normal</button>
+        <button onClick={() => navigate('gmap')} className="btn btn-primary w-full">Google Map</button>
+      </div>
     </div>
   )
 }
